34473cc8b6512d70516340518a610f1a4caf76ff
Raghu-KV <raghunandanv19@gmail.com>
2024-08-21 12:53:20 +0530

    for deployment

--- src/URL.js
+++ src/URL.js
@@ -1,2 +1,4 @@
-export const frontEndUrl = "http://localhost:3000";
-export const backEndUrl = "http://localhost:8080";
+// export const frontEndUrl = "http://localhost:3000";
+// export const backEndUrl = "http://localhost:8080";
+export const frontEndUrl = "https://questive-admin.netlify.app";
+export const backEndUrl = "https://exam-pro-be.vercel.app";
