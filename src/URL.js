3e76989023f1aab338212276473168356efd3bdc
Raghu-KV <raghunandanv19@gmail.com>
2024-08-23 10:41:36 +0530

    backend changed to render

--- src/URL.js
+++ src/URL.js
@@ -1,4 +1,7 @@
 // export const frontEndUrl = "http://localhost:3000";
 // export const backEndUrl = "http://localhost:8080";
+
 export const frontEndUrl = "https://questive-admin.netlify.app";
-export const backEndUrl = "https://exam-pro-be.vercel.app";
+
+// export const backEndUrl = "https://exam-pro-be.vercel.app";
+export const backEndUrl = "https://exam-pro-be.onrender.com";
